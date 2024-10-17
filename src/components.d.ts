17571bbacc30cd07381ed33f1e4722555c1e7815
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-10-17 20:17:49 +0200

    feat: add app-bar

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,7 +7,9 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    AppBar: typeof import('./components/AppBar.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    PageHeader: typeof import('./components/PageHeader.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
---
+++ src/components/AppBar.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const router = useRouter()
+</script>
+
+<template>
+  <v-app-bar
+    density="compact"
+    flat
+    color="transparent"
+  >
+    <v-app-bar-title>
+      <div
+        class="cursor-pointer"
+        @click="router.push('/')"
+      >
+        JN
+      </div>
+    </v-app-bar-title>
+    <template #append>
+      <v-btn
+        variant="plain"
+        text="Projects"
+        to="/projects"
+        class="custom-transform-class text-none"
+      />
+
+      <v-btn
+        size="small"
+        icon="mdi-application-outline"
+        to="/demos"
+      />
+      <v-btn
+        size="small"
+        icon="mdi-github"
+        href="https://github.com/jeffnawroth"
+      />
+      <v-btn
+        size="small"
+        icon="mdi-translate"
+      />
+
+      <v-btn
+        size="small"
+        icon="mdi-theme-light-dark"
+      />
+    </template>
+  </v-app-bar>
+</template>
