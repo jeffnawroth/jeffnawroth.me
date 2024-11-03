7a2bbfc0ec6b2017442e4db235b1ae79d460e137
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-11-03 15:20:18 +0100

    feat(layout): create home layout and update index page content

---
+++ src/layouts/home.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <AppBar />
+      <v-container max-width="800">
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,8 @@
+<route lang="yaml">
+  meta:
+    layout: home
+  </route>
+
 <script lang="ts" setup>
 //
 </script>
@@ -8,6 +13,39 @@
   </p>
 
   <p>
-    Hello, I am Jeff Nawroth, a master’s student in Business Informatics and a web developer from Germany.
+    Hi, I am Jeff Nawroth, a master’s student in <em>Business Informatics</em> and a web developer from Germany.
+  </p>
+
+  <p>
+    I am currently working on
+
+    <v-chip
+      label
+      href="https://github.com/jeffnawroth/source-taster"
+      variant="text"
+      :draggable="false"
+    >
+      <v-avatar
+        start
+        tile
+      >
+        <v-img
+          src="/src/assets/icon128.png"
+        />
+      </v-avatar>
+      The Source Taster
+    </v-chip>
+
+    as part of my master’s thesis. It is a Chrome extension designed to make it easier for students to verify the existence of literature sources.
+  </p>
+
+  <p>
+    Outside of programming and studying, I enjoy going to the gym, running marathons, and training for triathlons.
   </p>
 </template>
+
+<style>
+p {
+    margin: 1.5em 0;
+}
+</style>
