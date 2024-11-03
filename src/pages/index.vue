1ad95a80bf84bf9add23f5dd38b45ebf660242bf
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-11-03 13:50:12 +0100

    chore: simplify index page layout by removing v-card component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,18 +3,11 @@
 </script>
 
 <template>
-  <v-card
-    color="transparent"
-    flat
-    class="w-50 ma-auto"
-  >
-    <v-card-title class="text-h4 font-weight-black">
-      Jeff Nawroth
-    </v-card-title>
-    <v-card-text class="text-body-1">
-      <p>
-        Hello, I am Jeff Nawroth, a master’s student in Business Informatics and a web developer from Germany.
-      </p>
-    </v-card-text>
-  </v-card>
+  <p class="text-h4 font-weight-black">
+    Jeff Nawroth
+  </p>
+
+  <p>
+    Hello, I am Jeff Nawroth, a master’s student in Business Informatics and a web developer from Germany.
+  </p>
 </template>
